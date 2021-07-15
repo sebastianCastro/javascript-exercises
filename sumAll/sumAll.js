8234e96aa24fb2d0776b058b991408dcdf9bab6a
Sebastian <sebas.c1309@gmail.com>
2021-07-14 18:25:09 -0600

    finished sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(startingNum, EndingNum) {
+    if (typeof startingNum !== "number"|| typeof EndingNum !== "number") {
+        return "ERROR";
+    } else if (startingNum > EndingNum) {
+        temporalStart = EndingNum;
+        temporalEnd = startingNum;
+        startingNum = temporalStart;
+        EndingNum = temporalEnd;
+    }
+    result = 0;
+    for (let i = startingNum; i <= EndingNum; i++) {
+        result += i;
+    } if (result < 0) {
+        return "ERROR";
+    } else {
+        return result;
+    }
 };
+
+// console.log(sumAll(10, [90, 1]))
 
 module.exports = sumAll;
