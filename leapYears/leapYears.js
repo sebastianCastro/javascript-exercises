389fe4e9f6524807b5bee57f2ab6e166469e4477
Sebastian <sebas.c1309@gmail.com>
2021-07-15 17:54:31 -0600

    leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let fourHundred = year % 400;
+    let four = year % 4;
+    let hundred = year % 100;
 
+    if (fourHundred == 0) {
+        return true;
+    }
+    else if (four == 0 && hundred !== 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+console.log(leapYears(1997))
 
 module.exports = leapYears;
