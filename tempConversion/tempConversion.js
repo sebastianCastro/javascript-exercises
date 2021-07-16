5ab84831d04730ff71a8c1117547204faba7a111
Sebastian <sebas.c1309@gmail.com>
2021-07-15 18:35:27 -0600

    finished tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let takeAway = fahrenheit - 32;
+  let frac = 5 / 9;
+  let mult = takeAway * frac;
+  return Math.round(mult * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let frac = 9 / 5;
+  let mult = celsius * frac;
+  let addition = mult + 32;
+  return Math.round(addition * 10) / 10;
 };
 
 module.exports = {
