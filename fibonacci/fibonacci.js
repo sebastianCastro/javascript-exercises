cc4a067f5660abb2210ac6252b02c52489d1bc92
Sebastian <sebas.c1309@gmail.com>
2021-08-23 15:26:06 -0600

    solved fibonacci numbers

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    let fibonacciArray = [0,1];
+    let a = fibonacciArray[0];
+    let b = fibonacciArray[1];
+    let numberToBePush = 0;
+    while (fibonacciArray.length <= number) {
+        numberToBePush = fibonacciArray[a] + b;
+        fibonacciArray.push(numberToBePush);
+        a++;
+        b = numberToBePush;
+    }
+    return fibonacciArray[number];
 };
 
 module.exports = fibonacci;
