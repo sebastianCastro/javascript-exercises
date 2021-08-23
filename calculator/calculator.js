ef12d5dda328b40ddda4402a7c3f14555b16d42d
Sebastian <sebas.c1309@gmail.com>
2021-08-22 21:08:43 -0600

    fundamentals part 5

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let total = 0;
+  numbers.forEach(numberInArray => {
+    total += numberInArray;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  numbers.forEach(numberInArray => {
+    total *= numberInArray;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+  let total = 1;
+  total = numbers[0] ** numbers[1];
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  if (number > total) {
+    let i = number - 1;
+    total = number;
+    while (i > 1) {
+      total *= i;
+      i--;
+    }
+  } else {
+    return total;
+  }
+  return total;
 };
+
 
 module.exports = {
   add,
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let originalWord = Array.from(word);
+    let originalString = "";
+    let letters = Array.from(alphabet);
+    let reverseWord = "";
+    let i = word.length-1;
+    while (i >= 0) {
+        if (letters.includes(originalWord[i])) {
+            reverseWord += originalWord[i];
+            i--;
+        } else {
+            i--;
+        }
+    }
+    while (i <= word.length) {
+        if (letters.includes(originalWord[i])) {
+            originalString += originalWord[i];
+            i++;
+        } else {
+            i++;
+        }
+    }
+    if (reverseWord == originalString) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 module.exports = palindromes;
